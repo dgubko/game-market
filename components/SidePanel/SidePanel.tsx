60653c7b72f54438fc591e4ec1d9a7a269140d5c
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-04-17 18:49:20 -0600

    Add buttons

--- components/SidePanel/SidePanel.tsx
+++ components/SidePanel/SidePanel.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export const SidePanel = () => {
   return (
     <div className="side-panel">
       <h1 className="logo">GAME MARKET</h1>
+      <Link href="/">Wheel29</Link>
+      <Link href="/">Store</Link>
+      <Link href="/">Library</Link>
     </div>
   );
 };
