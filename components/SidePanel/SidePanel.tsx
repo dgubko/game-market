bd7c4dda8409f7bd407734655bc437782e18817a
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-04-20 20:41:19 -0600

    Update SidePanel styles

--- components/SidePanel/SidePanel.tsx
+++ components/SidePanel/SidePanel.tsx
@@ -1,12 +1,45 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+// import storeIcon from "@/lib/assets/store.svg";
+import { LibraryIcon, StoreIcon } from "@/components/assets";
 
 export const SidePanel = () => {
+  const router = useRouter();
+
+  const sideLinks = [
+    {
+      link: "/",
+      name: "Store",
+      icon: <StoreIcon />,
+    },
+    {
+      link: "/library",
+      name: "Library",
+      icon: <LibraryIcon />,
+    },
+  ];
+
   return (
     <div className="side-panel">
       <h1 className="logo">GAME MARKET</h1>
-      <Link href="/">Wheel29</Link>
-      <Link href="/">Store</Link>
-      <Link href="/">Library</Link>
+      <div className="user-info">
+        <div className="avatar-placeholder"></div>
+        <p>Wheel29</p>
+        <Link href="/">{">"}</Link>
+      </div>
+      <nav>
+        {sideLinks.map(({ icon, link, name }) => (
+          <Link
+            className={
+              router.pathname === link ? "active side-link" : "side-link"
+            }
+            href={link}
+          >
+            {icon}
+            {name}
+          </Link>
+        ))}
+      </nav>
     </div>
   );
 };
