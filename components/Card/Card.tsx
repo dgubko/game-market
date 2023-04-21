d463587ca3467a3f747e4affc71f64d8e9b79f31
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-04-20 20:41:35 -0600

    Update Card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,17 +1,25 @@
 import Link from "next/link";
 
 export const Card = (item) => {
+  const hasSalePrice = item.salePrice < item.price;
   return (
     <div className="card">
-      <img className="card-image" src={item.image} />
+      <div className="card-image">
+        <img src={item.image} alt={`${item.name} poster`} />
+      </div>
       <div className="card-description">
-        <h1 className="card-name">{item.name}</h1>
-        <div className="price-block">
-          <p>{!item.price ? "Free" : "$" + item.price}</p>
-          <Link className="buy-button" href={"/id"}>
-            Buy
-          </Link>
-        </div>
+        <h4 className="card-name">{item.name}</h4>
+      </div>
+      <div className="card-footer">
+        <span>
+          <p className={hasSalePrice ? "on-sale" : ""}>
+            {!item.price ? "Free" : "$" + item.price}
+          </p>
+          {hasSalePrice && <p>${item.salePrice}</p>}
+        </span>
+        <Link className="buy-button" href={"/id"}>
+          Buy
+        </Link>
       </div>
     </div>
   );
