f22ef0f1c054663ac3690fe28b9f63b9e9da0a95
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-05-11 09:42:05 -0600

    Update gamepage structure

--- pages/game/[id].tsx
+++ pages/game/[id].tsx
@@ -1,7 +1,11 @@
+import { SidePanel } from "@/components/SidePanel/SidePanel";
 import Head from "next/head";
+import PageLayout from "@/components/PageLayout/PageLayout";
 
 const GamePage = ({ game }) => {
   console.log(game);
+  const hasSalePrice = game.salePrice < game.price;
+
   return (
     <>
       <Head>
@@ -10,14 +14,40 @@ const GamePage = ({ game }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="game-page">
-        <p>{game.name}</p>
-        <img className="game-page-image" src={game.image} />
-        <p>{game.genre}</p>
-        <p>{game.description}</p>
-        <p>{game.developer}</p>
-        <p>{game.minSystemReq}</p>
-      </div>
+
+      <PageLayout>
+        <div className="game-page">
+          <div id="banner">
+            <img className="game-page-image" src={game.image} />
+            <div className="game-info">
+              <h1>{game.name}</h1>
+              <div>
+                <span>
+                  <p className={hasSalePrice ? "on-sale" : ""}>
+                    {!game.price ? "Free" : "$" + game.price}
+                  </p>
+                  {hasSalePrice && <p>${game.salePrice}</p>}
+                </span>
+                <button className="buy-button">Buy</button>
+              </div>
+            </div>
+          </div>
+          <div id="about">
+            <h2>About this game</h2>
+            <p>{game.genre}</p>
+            <p>{game.description}</p>
+            <p>{game.developer}</p>
+          </div>
+          <div id="requirements">
+            <h2>System Requirements</h2>
+            <p>{game.minSystemReq}</p>
+          </div>
+          <div id="reviews">
+            <h2>Reviews</h2>
+            <p>No reviews yet</p>
+          </div>
+        </div>
+      </PageLayout>
     </>
   );
 };
