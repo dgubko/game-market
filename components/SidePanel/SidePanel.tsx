8797eee57fabfcd4a1163d1bae2034ffae1e8d4a
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-05-15 09:24:28 -0600

    Update SidePanel for user

--- components/SidePanel/SidePanel.tsx
+++ components/SidePanel/SidePanel.tsx
@@ -1,7 +1,11 @@
+import React from "react";
 import Link from "next/link";
+import useSWR from "swr";
+import fetcher from "@/lib/fetcher";
 import { useRouter } from "next/router";
 // import storeIcon from "@/lib/assets/store.svg";
 import { LibraryIcon, StoreIcon } from "@/components/assets";
+import { User } from "@/types/api";
 
 export const SidePanel = () => {
   const router = useRouter();
@@ -16,30 +20,43 @@ export const SidePanel = () => {
       link: "/library",
       name: "Library",
       icon: <LibraryIcon />,
+      requiresUser: true,
     },
   ];
+
+  const [user, setUser] = React.useState<User>(undefined);
+  const {} = useSWR<{ user: User }>(`/api/user/Wheel29`, fetcher, {
+    onSuccess: (data) => {
+      setUser(data.user);
+    },
+  });
 
   return (
     <div className="side-panel">
       <h1 className="logo">GAME MARKET</h1>
-      <div className="user-info">
-        <div className="avatar-placeholder"></div>
-        <p>Wheel29</p>
-        <Link href="/">{">"}</Link>
-      </div>
+      {user && (
+        <div className="user-info">
+          <div className="avatar-placeholder"></div>
+          <p>{user.username}</p>
+          <Link href={`/profile/${user.username}`}>{">"}</Link>
+        </div>
+      )}
       <nav>
-        {sideLinks.map(({ icon, link, name }) => (
-          <Link
-            key={name}
-            className={
-              router.pathname === link ? "active side-link" : "side-link"
-            }
-            href={link}
-          >
-            {icon}
-            {name}
-          </Link>
-        ))}
+        {sideLinks.map(
+          ({ icon, link, name, requiresUser }) =>
+            (!requiresUser || user) && (
+              <Link
+                key={name}
+                className={
+                  router.pathname === link ? "active side-link" : "side-link"
+                }
+                href={link}
+              >
+                {icon}
+                {name}
+              </Link>
+            )
+        )}
       </nav>
     </div>
   );
--- types/api.d.ts
+++ types/api.d.ts
@@ -1,1 +1,13 @@
 export interface Game {}
+
+export interface User {
+  id: string;
+  email: string;
+  username: string;
+  name: string;
+  registerDate: string;
+  image?: string;
+  bio?: string;
+  location?: string;
+  emailVerified?: Boolean;
+}
